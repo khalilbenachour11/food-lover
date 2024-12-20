13ac98eb664c44dfe2084f8e7fa909e1269893d0
khalil ben achour <khalil.benachour@isgb.ucar.tn>
2024-12-20 20:50:42 +0100

    first commit

---
+++ style.js
@@ -0,0 +1,8 @@
+
+
+window.onload = function() {
+    setTimeout(function() {
+      alert("✨Bienvenue sur notre site internet ! Nous vous souhaitons une merveilleuse expérience ✨");
+    }, 500);
+  };
+  
